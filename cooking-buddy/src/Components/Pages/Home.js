027836b6e55065e85a25cff06d5d277bccef9eb6
Tom den Boer <tomdenboer@live.nl>
2022-10-29 11:58:54 +0200

    added a way to change background per comonent without scoped css

--- cooking-buddy/src/Components/Pages/Home.js
+++ cooking-buddy/src/Components/Pages/Home.js
@@ -1,8 +1,13 @@
 import styles from './Home.module.css';
 import Button from '../Common/CustomButton';
 import apiHelper from '../../helpers/apiUtils';
+import { useEffect } from 'react';
 
 function Home() {
+    // this effect is a decent way to set background dynamicly without scoped css. 
+    useEffect(() => {
+        document.body.classList.add(styles.body)
+    }, []);
     return (
         <main className={styles.main}>
             <h1 className={styles.title}>COOKING BUDDY</h1>
