1753b346cb89da6f072ba6fc5dd323aa0f6e3098
Tom den Boer <tomdenboer@live.nl>
2022-11-17 11:53:00 +0100

    added recipes to the recipe page, created a recipeListItem. Might have to do more styling to this later.

--- cooking-buddy/src/Components/Recipes/RecipeList.js
+++ cooking-buddy/src/Components/Recipes/RecipeList.js
@@ -1,15 +1,31 @@
+import { useEffect, useState } from 'react';
 import RecipeListItem from './RecipeListItem';
+import apiUtils from '../../helpers/apiUtils';
+
 
 function RecipeList(props) {
-    const list = props.recipes;
+    const [recipes, setRecipes] = useState([]);
+    useEffect(() => {
+        console.log("using effect");
+        const getRecipes = async () => {
+            const recipeList = await(apiUtils.test());
+            console.log(recipeList);
+            setRecipes(recipeList);
+        }
+        getRecipes();
+    }, []);
+
+    // const list = props.recipes;
     // console.log(list);
-    const listItems = list.map((item) =>
-        <RecipeListItem key={item.id} id={item.id} name={item.name} vegetarian={item.vegetarian}/>
+    const recipeListItems = recipes.map((item) =>
+        // <RecipeListItem key={item.id} id={item.id} name={item.title} vegetarian={item.vegetarian} healthScore={item.healthScore} img={item.image} timeToPrepare={item.readyInMinutes}/>
+        <RecipeListItem key={item.id} item={item}/>
     );
-    console.log(listItems);
+    // console.log(recipes);
+    // console.log(listItems);
     return (
         <ul>
-            {listItems}
+            {recipeListItems}
         </ul>
     )
 
--- cooking-buddy/src/Components/Recipes/RecipeListItem.js
+++ cooking-buddy/src/Components/Recipes/RecipeListItem.js
@@ -1,9 +1,19 @@
+import styles from './RecipeListItem.module.css';
+
 function RecipeListItem(props) {
+    const item = props.item;
     return (
-        <main>
-            <h3>{props.id}</h3>
-            <h2>{props.name}</h2>
-            <p>{props.vegetarian.toString()}</p>
+        <main className={styles.recipe}>
+            <img src={item.image} alt="picture of a recipe" className={styles.img}></img>
+            <div className={styles.recipeHeader}>
+                <h2>{item.title}</h2>
+            </div>
+            <div className={styles.recipeInfo}>
+                <p>Health score: {item.healthScore}</p>
+                <p>{item.vegetarian.toString()}</p>
+                <p>Preparation time: {item.readyInMinutes}</p>
+            </div>
+            
         </main>
         
     )
